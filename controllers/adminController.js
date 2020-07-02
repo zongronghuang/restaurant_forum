01c9162b281eac58db4faa18d05475cb19d5432e
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-07-03 00:55:24 +0800

    feat: create admin restaurant create form and route

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,9 +5,40 @@ const adminController = {
   getRestaurants: (req, res) => {
     return Restaurant.findAll({ raw: true })
       .then(restaurants => {
-        return res.render('admin/restaurants', { restaurants })
+        return res.render('admin/restaurants', {
+          restaurants,
+          user: req.user,
+          isAuthenticated: req.isAuthenticated
+        })
       })
-  }
+  },
+
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    const { name, tel, address, opening_hours, description } = req.body
+
+    if (!name) {
+      req.flash('error_messages', 'No restaurant info')
+
+      return res.redirect('back')
+    }
+
+    return Restaurant.create({
+      name,
+      tel,
+      address,
+      opening_hours,
+      description
+    })
+      .then(restaurant => {
+        req.flash('success_messages', 'Created a restaurant item')
+        res.redirect('/admin/restaurants')
+      })
+  },
+
 }
 
 module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 
 module.exports = (app, passport) => {
-  // 一般使用者認證方法
+  // 一般使用者驗證
   const authenticated = (req, res, next) => {
     if (req.isAuthenticated()) { return next() }
 
     res.redirect('/signin')
   }
 
-  // Admin 認證方法
+  // Admin 驗證
   const authenticatedAdmin = (req, res, next) => {
     if (req.isAuthenticated()) {
       if (req.user.isAdmin) { return next() }
@@ -20,28 +20,36 @@ module.exports = (app, passport) => {
     res.redirect('/signin')
   }
 
-  // 通過認證前往首頁路由
+  ////////// 通過驗證前往首頁路由 \\\\\\\\\\
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+
   app.get('/restaurants', authenticated, restController.getRestaurants)
 
-  // Admin 路由
+  //////////// Admin 管理路由 \\\\\\\\\\
   app.get('/admin', authenticatedAdmin, (req, res) => {
     res.redirect('/admin/restaurants')
   })
+
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
-  // 註冊路由
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
+
+  app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
+
+  ////////// 使用者註冊路由 \\\\\\\\\\
   app.get('/signup', userController.getSignUpPage)
+
   app.post('/signup', userController.signUp)
 
-  // 登入路由
+  ////////// 使用者登入路由 \\\\\\\\\\
   app.get('/signin', userController.getSignInPage)
+
   app.post('/signin', passport.authenticate('local', {
     failureRedirect: '/',
     failureFlash: true
   }), userController.signIn)
 
-  // 登出路由
+  ////////// 使用者登出路由 \\\\\\\\\\
   app.get('/logout', userController.logOut)
 
 }
