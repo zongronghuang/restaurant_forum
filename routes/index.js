a50203a1bac82e30abfb930bd07a30f4caa4654a
HUANG Zong-Rong <maibells0@yahoo.com.tw>
2020-07-02 17:42:28 +0800

    feat: create isAdmin auth logic and add isAdmin auth logic to routes

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,45 @@ const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 
 module.exports = (app, passport) => {
-  app.get('/', (req, res) => res.redirect('/restaurants'))
+  // 一般使用者認證方法
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) { return next() }
 
-  app.get('/restaurants', restController.getRestaurants)
+    res.redirect('/signin')
+  }
 
-  app.get('/admin', (req, res) => {
+  // Admin 認證方法
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
+
+  // 通過認證前往首頁路由
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticated, restController.getRestaurants)
+
+  // Admin 路由
+  app.get('/admin', authenticatedAdmin, (req, res) => {
     res.redirect('/admin/restaurants')
   })
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
-  app.get('/admin/restaurants', adminController.getRestaurants)
-
-
+  // 註冊路由
   app.get('/signup', userController.getSignUpPage)
   app.post('/signup', userController.signUp)
 
+  // 登入路由
   app.get('/signin', userController.getSignInPage)
   app.post('/signin', passport.authenticate('local', {
     failureRedirect: '/',
     failureFlash: true
   }), userController.signIn)
+
+  // 登出路由
   app.get('/logout', userController.logOut)
 
 }
