297d4a22b461cb59916ed92808235fc4771bfc6e
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-07-06 00:09:49 +0800

    refactor: add env variable for port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport.js')
 const methodOverride = require('method-override')
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
