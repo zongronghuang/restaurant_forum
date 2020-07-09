72b09ad4e3619b6deab6722358cad0c0a46257de
HUANG Zong-Rong <maibells0@yahoo.com.tw>
2020-07-09 17:59:39 +0800

    feat: attempt pagination in restController

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,25 +1,40 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
+
 
 const restController = {
   getRestaurants: (req, res) => {
+    let offset = 0
     let whereQuery = {}
     let categoryId = ''
+
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
 
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery['CategoryId'] = categoryId
     }
 
-    Restaurant.findAll({
+    Restaurant.findAndCountAll({
       raw: true,
       nest: true,
       include: [Category],
-      where: whereQuery
+      where: whereQuery,
+      offset,
+      limit: pageLimit
     })
-      .then(restaurants => {
-        const data = restaurants.map(r => ({
+      .then(result => {
+        let page = Number(req.query.page) || 1
+        let pages = Math.ceil(result.count / pageLimit)
+        let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+        let prev = (page - 1) < 1 ? 1 : (page - 1)
+        let next = (page + 1) > pages ? pages : (page + 1)
+
+        const data = result.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50),
           categoryName: r.Category.name
@@ -33,7 +48,11 @@ const restController = {
             return res.render('restaurants', {
               restaurants: data,
               categories,
-              categoryId
+              categoryId,
+              page,
+              totalPage,
+              prev,
+              next
             })
           })
       })
